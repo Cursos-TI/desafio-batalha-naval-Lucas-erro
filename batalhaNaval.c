cb07972c0fcb46b518aa2ca9b83709d703833fc7
Lucas-erro <cristiancaue36@gmail.com>
2025-05-29 22:53:22 +0000

    defini o tamanho do tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,5 +2,17 @@
     int main(){
     printf("Batalha Naval Novato!");
 
+    #define TAMANHO_TABULEIRO 10  // Define o tamanho fixo do tabuleiro
+    #define TAMANHO_NAVIO 3       // Define o tamanho fixo dos navios
+
+    int main() {
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];  // Matriz que representa o tabuleiro
+    // 1. Inicializando o tabuleiro com 0 (água)
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            tabuleiro[i][j] = 0;  // 0 representa água
+        }
+    }    
+
     return 0;
 }
